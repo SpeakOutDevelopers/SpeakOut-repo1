1f28c637ec03e5e8a5b402234f93d01764d6d511
Luis Alejandro Tróchez <zehcort@hotmail.es>
2017-05-25 17:57:16 -0500

    Imágenes en registro y cambios en elementos intro

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -27,10 +27,9 @@ export class IntroPage {
   @ViewChild('slides') slides: Slides;
 
   images = [
-    'https://s-media-cache-ak0.pinimg.com/736x/30/6f/8b/306f8b543809aa6eb7839a27812d8b50.jpg',
-    'http://www.joelfloraphotography.com/wp-content/uploads/2016/01/1452052623-400x400.jpg',
-    'https://68.media.tumblr.com/afa0c34cca805e9e79d0e76cc5681379/tumblr_onbo3aA9tV1re67v3o1_400.jpg',
-    'https://images.8tracks.com/cover/i/008/562/610/Bad_Bitch-4914.jpg?rect=42,0,400,400&q=98&fm=jpg&fit=max'
+    '../../assets/img/intro1.jpg',
+    '../../assets/img/intro2.jpg',
+    '../../assets/img/intro3.jpg'
   ];
   user: Usuario = {
     nombre:"",
